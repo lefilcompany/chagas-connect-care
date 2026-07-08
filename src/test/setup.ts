6bb1691c7572bf10bcddf23f737cee830ab9722b
lefilcompany <lefil@lefil.com.br>
2026-07-08 09:33:31 -0300

    test: ampliar setup compartilhado do Vitest

--- src/test/setup.ts
+++ src/test/setup.ts
@@ -1,4 +1,6 @@
 import "@testing-library/jest-dom";
+import { cleanup } from "@testing-library/react";
+import { afterEach, vi } from "vitest";
 
 Object.defineProperty(window, "matchMedia", {
   writable: true,
@@ -10,6 +12,43 @@ Object.defineProperty(window, "matchMedia", {
     removeListener: () => {},
     addEventListener: () => {},
     removeEventListener: () => {},
-    dispatchEvent: () => {},
+    dispatchEvent: () => false,
   }),
+});
+
+class ResizeObserverMock {
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+}
+
+class IntersectionObserverMock {
+  root = null;
+  rootMargin = "0px";
+  thresholds = [0];
+  observe() {}
+  unobserve() {}
+  disconnect() {}
+  takeRecords() { return []; }
+}
+
+Object.defineProperty(window, "ResizeObserver", { writable: true, value: ResizeObserverMock });
+Object.defineProperty(window, "IntersectionObserver", { writable: true, value: IntersectionObserverMock });
+Object.defineProperty(Element.prototype, "scrollIntoView", { writable: true, value: vi.fn() });
+
+if (!window.HTMLElement.prototype.hasPointerCapture) {
+  window.HTMLElement.prototype.hasPointerCapture = () => false;
+}
+if (!window.HTMLElement.prototype.setPointerCapture) {
+  window.HTMLElement.prototype.setPointerCapture = () => {};
+}
+if (!window.HTMLElement.prototype.releasePointerCapture) {
+  window.HTMLElement.prototype.releasePointerCapture = () => {};
+}
+
+afterEach(() => {
+  cleanup();
+  window.localStorage.clear();
+  window.sessionStorage.clear();
+  vi.restoreAllMocks();
 });
